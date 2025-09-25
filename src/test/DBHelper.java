af87c0f0537de1aaacb0c14ec941d09f39e67263
Aleeex3 <a.garciasanz@colegiovirgendeeuropa.edu.es>
2025-09-25 13:43:17 +0200

    second commit

--- src/test/DBHelper.java
+++ src/test/DBHelper.java
@@ -8,20 +8,20 @@ import java.sql.SQLException;
 public class DBHelper {
 	//The direction of the database
 	//JDBC stands for java database connectivity
-	String dbURL = "jdbc:mysql://localhost:3306/JDBC";
-	String username = "root";
-	String password = "admin";
+	private static String dbURL = "jdbc:mysql://localhost:3306/JDBC";
+	private static String username = "root";
+	private static String password = "admin";
 	//This class is used to manage different drivers for relational database
 	DriverManager driverManager;
 	//Creamos una conneccion con la base de datos MySQL
-	public Connection getConnection(){
+	public static Connection getConnection(){
 		try {
 			return DriverManager.getConnection(dbURL, username, password);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return null;
+		return null; 
 }
 	
 }
--- src/test/UserDao.java
+++ src/test/UserDao.java
@@ -1,5 +1,25 @@
 package test;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * data access object for user table
+ *  PC-ProfesorCVE 25 sept 2025
+ */
 public class UserDao {
+	public void insert(int id, String name, boolean isVIP) {
+	String insertSQL = "insert into users values ( "+"'alex'," + "'admin'," + "1, "+"12)";
+	Connection connection = DBHelper.getConnection();
+	try {
+		PreparedStatement ps = connection.prepareStatement(insertSQL);
+		ps.executeUpdate();
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 
 }
+}
+ 
